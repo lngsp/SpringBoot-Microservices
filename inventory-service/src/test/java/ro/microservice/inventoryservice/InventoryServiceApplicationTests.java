902b767fa4ce7a1da80fea184ecde47ece11d76d
stefania-lungu <100295262+stefania-lungu@users.noreply.github.com>
2024-02-26 15:51:24 +0200

    Create Inventory Service

---
+++ inventory-service/src/test/java/ro/microservice/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.microservice.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
